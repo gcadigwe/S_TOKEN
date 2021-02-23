048dd18a2b0540036986a768f28d4b1f29149291
GCAdigwe <gcadigwe@gmail.com>
2021-02-23 14:25:04 +0000

    added transfer feature to the smart contract and wrote tests for them

--- contracts/SarzyToken.sol
+++ contracts/SarzyToken.sol
@@ -11,6 +11,14 @@ contract SarzyToken {
 	//token standard
 	string public standard = "Sarzy Token v1.0";
 
+	//Transfer event
+	event Transfer (
+		address indexed _from,
+		address indexed _to,
+		uint indexed _value
+		);
+
+
 	//getting balance
 	mapping(address => uint) public balanceOf;
 
@@ -19,5 +27,23 @@ contract SarzyToken {
 
 		 //allocate the initial supply
 		 balanceOf[msg.sender] = _initialsupply;
+	}
+
+	//Transfer
+
+	function transfer(address _to, uint _value) public returns (bool success) {
+	// Exception if account doesn't have enough token
+	require(balanceOf[msg.sender] >= _value);
+	//Transfer balance
+	balanceOf[msg.sender] -= _value;
+	balanceOf[_to] += _value;
+
+	//Transfer Event
+
+	emit Transfer(msg.sender, _to, _value);
+
+	//Return a boolean
+	return true;
+	
 	}
 }
